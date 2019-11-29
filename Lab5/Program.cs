b2daf5bdfd34fef862659321d8b78b0612c36a11
Danil Dubrovsky <forwot163@gmail.com>
2019-11-29 11:30:02 +0300

    Finished lab 5

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -3,6 +3,13 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
 
 namespace Lab5
 {
@@ -44,7 +51,7 @@ namespace Lab5
 
         public override string ToString()
         {
-            return $"{nameof(A)}: {A}, {nameof(B)}: {B}, {nameof(C)}: {C}";
+            return $"{nameof(A)}: {A}\n, {nameof(B)}: {B}\n, {nameof(C)}: {C}\n";
         }
     }
 
@@ -95,20 +102,92 @@ namespace Lab5
             new BinaryFormatter().Serialize(stream, value);
         }
     }
+
+    class SqlTriangleSerializer
+    {
+        public readonly MySqlConnection Connection;
+
+        public SqlTriangleSerializer(MySqlConnection connection)
+        {
+            this.Connection = connection;
+            new MySqlCommand(@"create table if not exists triangles
+            (
+                id int auto_increment,
+                ax float null,
+            ay float null,
+            az float null,
+            bx float null,
+           `by` float null,
+            bz float null,
+            cx float null,
+            cy float null,
+            cz float null,
+            constraint triangles_pk
+            primary key (id)
+                );
+
+            ", connection).ExecuteNonQuery();
+        }
+        public void AddTriangle(Triangle triangle)
+        {
+            new MySqlCommand($@"INSERT INTO 
+             triangles(ax,ay,az,bx,`by`,bz,cx,cy,cz) 
+             VALUES({triangle.A.X},{triangle.A.Y},{triangle.A.Z},
+            {triangle.B.X},{triangle.B.Y},{triangle.B.Z},
+            {triangle.C.X},{triangle.C.Y},{triangle.C.Z})", Connection).ExecuteNonQuery();
+        }
+
+        public List<Triangle> ReadAllTriangles()
+        {
+            var reader = new MySqlCommand(@"SELECT * FROM triangles", 
+                Connection).ExecuteReader();
+            var results = new List<Triangle>();
+            while (reader.Read())
+            {
+                results.Add(new Triangle(new Point(reader.GetFloat("ax"),
+                        reader.GetFloat("ay"),reader.GetFloat("az" )),
+                    new Point(reader.GetFloat("bx"),reader.GetFloat("by"),
+                        reader.GetFloat("bz" )),
+                    new Point(reader.GetFloat("cx"),reader.GetFloat("cy"),
+                        reader.GetFloat("cz" ))));
+            }
+
+            return results;
+        }
+    }
+  
     
     class Program
     {
+        
         static void Main(string[] args)
         {
             var triangle = new Triangle(new Point(0, 0, 0), 
                 new Point(0, 1, 0), 
                 new Point(1, 0, 0) );
 
+            Console.WriteLine("Writing to xml:");
             var serializer = new XmlCustomSerializer<Triangle>();
             serializer.Write("Triangle.xml", triangle);
             var deserialized = serializer.Read("Triangle.xml");
-            
             Console.WriteLine(deserialized);
+            
+            Console.WriteLine("Writing to binary:");
+            var binarySerializer = new BinarySerializer<Triangle>();
+            binarySerializer.Write("Triangle.bin", triangle);
+            var deserializedBinary = binarySerializer.Read("Triangle.bin");
+            Console.WriteLine(deserializedBinary);
+
+            Console.WriteLine("Writing to database:");
+            var connection = new MySqlConnection($@"Server=localhost; database=labs; UID=root; password=918273645;");
+            connection.Open();
+            var sqlTriangleSerializer = new SqlTriangleSerializer(connection);
+            sqlTriangleSerializer.AddTriangle(triangle);
+            var triangles = sqlTriangleSerializer.ReadAllTriangles();
+            foreach (var loadedFromSql in triangles)
+            {
+                Console.WriteLine(loadedFromSql);
+            }
         }
     }
 }
